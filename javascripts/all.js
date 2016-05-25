627c6cd02da07caa463042e0d946ae180bb0ed03
Maurice <mcsmorenberg@gmail.com>
2016-05-25 12:46:58 +0200

    Automated commit at 2016-05-25 10:46:58 UTC by middleman-deploy 2.0.0-alpha

--- javascripts/all.js
+++ javascripts/all.js
@@ -1,11 +1,16 @@
 // This is where it all goes :)
 (function(){
-  var welcome1 = document.querySelector('.welcomeHeader'),
-      welcome2 = document.querySelector('.welcomeBody');
+  var welcome1 = document.querySelector('.welcomeHeader');
+  var welcome2 = document.querySelector('.welcomeBody');
 
   // Welcome text onload
   setTimeout(function(){
     welcome1.classList.add("has-landed");
+
+  }, 900);
+
+  setTimeout(function(){
+
     welcome2.classlist.add("has-landed-to");
   }, 900);
 })();
