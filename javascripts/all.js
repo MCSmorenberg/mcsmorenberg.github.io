d66429cbc835ac680ced6ba6fe8de856ae63d386
Maurice <mcsmorenberg@gmail.com>
2016-05-25 12:25:49 +0200

    Automated commit at 2016-05-25 10:25:49 UTC by middleman-deploy 2.0.0-alpha

--- javascripts/all.js
+++ javascripts/all.js
@@ -1,1 +1,9 @@
 // This is where it all goes :)
+(function(){
+  var welcome = document.querySelector('.welcomeHeader');
+  console.log(welcome);
+  // Welcome text onload
+  setTimeout(function(){
+    welcome.classList.add("has-landed");
+  }, 500);
+})();
